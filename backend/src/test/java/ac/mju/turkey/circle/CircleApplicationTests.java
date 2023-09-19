9fd3fbaa4c1e757ee08c78373f8b28d0f48a8ca1
MyungHyun Chun <cmh1448@naver.com>
2023-09-19 17:18:54 +0900

    chore(be): backend project init

---
+++ backend/src/test/java/ac/mju/turkey/circle/CircleApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.mju.turkey.circle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
