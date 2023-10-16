feaa348ef55ae0a6eed74432458ceac2d33117ca
MyungHyun Chun <cmh1448@naver.com>
2023-10-16 19:59:29 +0900

    fix(fe): 데스크톱 뷰 오류 수정

--- frontend/src/components/pages/PageContainer.tsx
+++ frontend/src/components/pages/PageContainer.tsx
@@ -7,7 +7,7 @@ export interface PageContainerProps {
 export default function PageContainer(props: PageContainerProps) {
   return (
     <div className="w-full px-4 flex justify-center overflow-y-auto apply-scrollbar">
-      <div className="w-full">
+      <div className="w-full flex flex-col items-center">
         <div className="w-full pt-[50px] md:w-[700px] lg:w-[900px] xl:w-[1200px]">
           {props.children}
         </div>
