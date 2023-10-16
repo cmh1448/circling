a776ff55dbe1f2865692d114f865361059800af9
MyungHyun Chun <cmh1448@naver.com>
2023-10-16 11:14:39 +0900

    fix(fe): 오타 수정

--- frontend/src/features/circle/components/CircleItem.tsx
+++ frontend/src/features/circle/components/CircleItem.tsx
@@ -16,12 +16,12 @@ export default function CircleItem() {
   return (
     <div className=" bg-gray-100 p-2 rounded-lg">
       <div className="flex items-center">
-        <dsv
+        <div
           onClick={() => handleClickCircle(1)}
           className="text-lg font-bold bg-transparent w-full hover:bg-gray-200 active:scale-[0.98] p-1 rounded-lg transition-all cursor-pointer select-none"
         >
           음악 동아리
-        </dsv>
+        </div>
       </div>
       <div className="flex flex-col mt-2">
         {[1, 2, 3, 4, 5].map(() => {
