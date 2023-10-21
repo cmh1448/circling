62bd73aca7b5fbf25c3f88467ee627772eb7cbdd
천명현 <cmh1448@naver.com>
2023-10-21 18:13:19 +0900

    fix(be): 서버 시간대 고정

--- backend/src/main/java/ac/mju/turkey/circle/CircleApplication.java
+++ backend/src/main/java/ac/mju/turkey/circle/CircleApplication.java
@@ -1,15 +1,21 @@
 package ac.mju.turkey.circle;
 
-import ac.mju.turkey.circle.system.auditor.CircleAuditorAware;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 @EnableJpaAuditing(auditorAwareRef = "circleAuditorAware")
 public class CircleApplication {
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CircleApplication.class, args);
