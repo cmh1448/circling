5b9a0e8b61e4b9f0f3666442b3edc3133b936afc
천명현 <cmh1448@naver.com>
2025-03-14 00:34:04 +0900

    fix: update database and Redis configuration for containerized environment

--- backend/src/main/java/ac/mju/turkey/circle/system/configuration/RedisConfig.java
+++ backend/src/main/java/ac/mju/turkey/circle/system/configuration/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -21,9 +22,15 @@ public class RedisConfig {
     @Value("${spring.redis.source.port}")
     private int port;
 
+    @Value("${spring.redis.source.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+        redisConfig.setPassword(password);
+
+        return new LettuceConnectionFactory(redisConfig);
     }
 
     @Bean
